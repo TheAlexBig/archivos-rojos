b0c95bf2e827e18ec370356666472488ef1f2e01
Jose Valladares <00062816@uca.edu.sv>
2023-04-15 16:59:26 -0600

    single library usage

--- src/connector/mysql.js
+++ src/connector/mysql.js
@@ -1,11 +1,12 @@
-import mysql from 'mysql';
+import mysql from 'mysql2/promise';
 import connectionDetails from '../env-reader/index.js';
 
 export default () => {
-    return mysql.createConnection({
+    return mysql.createPool({
         host: connectionDetails.host,
         user: connectionDetails.user,
         password: connectionDetails.password,
         database: connectionDetails.database,
+        connectionLimit: connectionDetails.connectionLimit
     });
 }
