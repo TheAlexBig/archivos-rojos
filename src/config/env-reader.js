0fc02beee04025ba5f63fde9b8656dd6cf0539d6
Jose Valladares <00062816@uca.edu.sv>
2023-04-29 16:20:39 -0600

    Use different env

--- src/config/env-reader.js
+++ src/config/env-reader.js
@@ -4,13 +4,13 @@ dotenv.config();
 
 // Access environment variables
 const connectionDetails = {
-    connectionType: process.env.CONNECTION_TYPE || 'postgres',
-    user:  process.env.USER || 'postgres',
-    host:  process.env.HOST || 'localhost',
-    database:  process.env.DATABASE || 'postgres',
-    password:  process.env.PASSWORD || 'postgres',
-    port: process.env.PORT || 5432,
-    connectionLimit:  process.env.CONNECTION_LIMIT || 10,
+    connectionType: process.env.CONNECTION_TYPE || 'mysql',
+    user:  process.env.MYSQL_USER || 'postgres',
+    host:  process.env.MYSQL_HOST || 'localhost',
+    database:  process.env.MYSQL_DATABASE || 'postgres',
+    password:  process.env.MYSQL_PASSWORD || 'postgres',
+    port: process.env.MYSQL_DB_PORT || 5432,
+    connectionLimit:  process.env.BACKEND_CONNECTION_LIMIT || 10,
 }
 
 export default connectionDetails;
