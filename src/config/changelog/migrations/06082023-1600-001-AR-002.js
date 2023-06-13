efecfdb22ce71a1998c5de095484f7b41b22252a
Jose Valladares <00062816@uca.edu.sv>
2023-06-13 01:33:13 -0600

    fix migrations

--- src/config/changelog/migrations/06082023-1600-001-AR-002.js
+++ src/config/changelog/migrations/06082023-1600-001-AR-002.js
@@ -4,11 +4,10 @@ export default {
   description: 'Create users table',
   script: `
     CREATE TABLE IF NOT EXISTS users (
-       id INT AUTO_INCREMENT PRIMARY KEY,
-       name VARCHAR(50),
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(50),
       email VARCHAR(255),
       created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-    );
-    INSERT INTO users (name, email) VALUES ('Support User', 'support@mail.com');
+  );
   `
 }
---
+++ src/config/changelog/migrations/06082023-1600-001-AR-003 copy.js
@@ -0,0 +1,6 @@
+export default {
+  identifier: "06082023-1600-001-AR-003",
+  version: 3,
+  description: 'Default entry',
+  script: `INSERT INTO users (name, email) VALUES ('Support User', 'support@mail.com');`
+}
